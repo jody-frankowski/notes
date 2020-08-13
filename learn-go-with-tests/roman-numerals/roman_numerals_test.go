4eaa1bc245a644068bd01346ad9d4b9b638d6a79
Jody Frankowski <jody.frankowski@gmail.com>
2020-08-13 22:13:32 +0200

    go: Add "Context" and "Intro to property based tests" chapters

---
+++ learn-go-with-tests/roman-numerals/roman_numerals_test.go
@@ -0,0 +1,27 @@
+package roman_numerals
+
+import (
+	"testing"
+	"testing/quick"
+)
+
+func TestRomanNumerals(t *testing.T) {
+	got := ConvertToRoman(1)
+	want := "I"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
+	}
+}
