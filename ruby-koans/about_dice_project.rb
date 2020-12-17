22f2eb543d439447683a5cfc7969b57e27ee8565
Jody Frankowski <jody.frankowski@gmail.com>
2020-12-17 13:07:31 +0100

    ruby: Add progress

--- ruby-koans/about_dice_project.rb
+++ ruby-koans/about_dice_project.rb
@@ -6,6 +6,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_accessor :values
+
+  def roll(number_of_rolls)
+    @values = []
+    number_of_rolls.times do
+      @values << rand(1..6)
+    end
+  end
+
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
